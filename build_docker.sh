3f54ae049bcb6856af4f79b9f2ab3f360967c135
senioravanti <senioravanti@vk.com>
2025-09-13 20:02:13 +0300

    chore: init repo

---
+++ build_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -e
+clear
+
+PLATFORM='linux/amd64'
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -t|--tag) TAG="$2"; shift 2;;
+    -p|--platform) PLATFORM="$2"; shift 2;;
+    -o|--other-options) OTHER_OPTIONS="$2"; shift 2;;
+    --no-cache) IS_NO_CACHE='--no-cache'; shift;;
+    --push) IS_PUSH='--push'; shift;;
+    *) echo 'unknown argument'; exit 1;;
+  esac
+done
+
+docker build . \
+  -f ./docker/Dockerfile \
+  --platform "$PLATFORM" -t "$TAG" \
+  $IS_NO_CACHE $IS_PUSH $OTHER_OPTIONS
