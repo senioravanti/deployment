dbea0b13cc1f156e00dff06b774ef05996e08662
senioravanti <senioravanti@vk.com>
2025-09-13 21:27:41 +0300

    fix: externalize dockerfile path and project dir

--- build_docker.sh
+++ build_docker.sh
@@ -9,13 +9,15 @@ while [ $# -gt 0 ]; do
     -t|--tag) TAG="$2"; shift 2;;
     -p|--platform) PLATFORM="$2"; shift 2;;
     -o|--other-options) OTHER_OPTIONS="$2"; shift 2;;
+    -d|--dockerfile) DOCKERFILE_PATH="$2"; shift 2;;
+    --project-dir) PROJECT_DIR="$2"; shift 2;;
     --no-cache) IS_NO_CACHE='--no-cache'; shift;;
     --push) IS_PUSH='--push'; shift;;
     *) echo 'unknown argument'; exit 1;;
   esac
 done
 
-docker build . \
-  -f ./docker/Dockerfile \
+docker build "$PROJECT_DIR" \
+  -f "$DOCKERFILE_PATH" \
   --platform "$PLATFORM" -t "$TAG" \
   $IS_NO_CACHE $IS_PUSH $OTHER_OPTIONS
